3556a0ee6d06426e51b223c5fc152a30d4d1504d
Audun Hammer Hovda <hammerhovda@gmail.com>
2022-12-22 01:05:33 +0100

    initial commit

---
+++ pages/party.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Alle partier</h1>
+        <ul>
+            <li v-for="party in parties" :key="party.id">
+                <a :href="`/party/${party.id}`">{{ party.name }}{{ party.abbreviation ? ` (${party.abbreviation})` : '' }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const supabase = useSupabaseClient()
+
+let { data: parties, error } = await supabase
+    .from('party')
+    .select('*')
+    .order('id', { ascending: true })
+
+</script>
